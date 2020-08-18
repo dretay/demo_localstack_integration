03b815983d5d170beb293c5fdb854e9819ea4e18
Drew Tayman <dretay@gmail.com>
2020-08-18 11:36:32 -0400

    update to sql

--- psql/init.sql
+++ psql/init.sql
@@ -1,2 +1,3 @@
+CREATE USER demo_localstack_user WITH PASSWORD 'demo';
 CREATE DATABASE demo_localstack_application;
-GRANT ALL PRIVILEGES ON DATABASE demo_localstack_application TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE demo_localstack_application TO demo_localstack_user;
